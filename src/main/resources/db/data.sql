d34054bf20f18f65bdf34e40bf7e2eaf5d6b2163
TaengGyul <mjcho0130@gmail.com>
2025-06-03 11:11:39 +0900

    더미데이터 생성

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,8 @@
+insert into user_tb (name, id)
+values ('ssar', 1);
+insert into user_tb (name, id)
+values ('cos', 2);
+insert into user_tb (name, id)
+values ('haha', 3);
+insert into user_tb (name, id)
+values ('love', 4);
